f05c1ccbaa0d236cd8f175660a783baae0db495e
Alexis Dávila <alexisdavila@assertsolutions.com>
2024-03-27 03:27:45 -0300

    Fix obtener ultimos mensajes + decode utf-8

--- src/webadmin/webadmin.service.ts
+++ src/webadmin/webadmin.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@nestjs/common";
-import * as process from "process";
-import { Commons } from "../commons/commons";
-import axios from "axios";
 import { Cron } from "@nestjs/schedule";
+
+import * as process from "process";
+import axios from "axios";
+import * as iconv from 'iconv-lite';
+
+import { Commons } from "../commons/commons";
 
 @Injectable()
 export class WebadminService {
@@ -18,6 +21,7 @@ export class WebadminService {
   }
 
   async getConnection(){
+
     const url = this.baseUrl;
     const credentials = this.commons.encodeToBase64(`${this.userAdmin}:${this.password}`);
 
@@ -27,6 +31,7 @@ export class WebadminService {
 
     try {
       const response = await axios.get(url, { headers });
+
       // return response.data;
       if (response.status === 200){
         return true
@@ -41,40 +46,16 @@ export class WebadminService {
 
   @Cron('*/5 * * * * *')
   async cronDataLogs(){
-    console.log("Se ejecuta cron");
     let data= await this.dataLogs();
-    // data = this.commons.decodeParams(data);
-    let logsList: string[] = this.commons.splitLines(data);
-    // logsList = await this.guardarYObtenerMensajesNuevos(logsList);
-    console.log(logsList)
-    this.cache = await this.newMessages(logsList);
-    return this.dataLogs();
+    const forLogs = await this.newMessages(data);
+    console.log(forLogs);
+    // return await this.newMessages(data)
+    // return this.dataLogs();
   }
 
-  // async dataLogs():Promise<string>{
-  //   const url = `${this.baseUrl}/current_console_log`;
-  //   const credentials = this.commons.encodeToBase64(`${this.userAdmin}:${this.password}`);
-  //
-  //   const headers = {
-  //     'Authorization': `Basic ${credentials}`,
-  //   };
-  //
-  //   try {
-  //     const response = await axios.get(url, { headers });
-  //     return response.data;
-  //     // if (response.status === 200){
-  //     //   return true
-  //     // }
-  //
-  //   } catch (error) {
-  //     console.log(error)
-  //     return error
-  //   }
-  // }
-
-
-  async dataLogs(): Promise<string> {
+  async dataLogs(): Promise<string[]> {
     const url = `${this.baseUrl}/current_console_log`;
+    axios.defaults.responseEncoding= 'utf8';
     const credentials = this.commons.encodeToBase64(`${this.userAdmin}:${this.password}`);
 
     const headers = {
@@ -82,8 +63,11 @@ export class WebadminService {
     };
 
     try {
-      const response = await axios.get(url, { headers });
-      const consoleLogHtml = response.data;
+
+      const response = await axios.get(url, { headers, responseType: 'arraybuffer' });
+      const decodedData = iconv.decode(response.data, 'iso-8859-1'); // Decode from iso-8859-1 to UTF-8
+      const consoleLogHtml = decodedData.toString(); // Convert to string
+
       const startTag = '&gt;';
       const endTag = '<a name="END"></a>';
       const startIndex = consoleLogHtml.indexOf(startTag);
@@ -92,8 +76,9 @@ export class WebadminService {
       if (startIndex !== -1 && endIndex !== -1) {
         consoleLogText = consoleLogHtml.substring(startIndex + startTag.length, endIndex).trim();
         consoleLogText = this.commons.decodeParams(consoleLogText);
+
       }
-        return consoleLogText;
+        return this.commons.splitLines(consoleLogText);
         //throw new Error('No se pudo encontrar el contenido de la consola en la respuesta HTML.');
 
     } catch (error) {
@@ -101,36 +86,29 @@ export class WebadminService {
       throw error;
     }
   }
-  async newMessages(messages: string[]){
-    let newMessageList: string[] = [];
 
-    if (this.cache.length === 0 || messages.every((element, index) => element === this.cache[index])){
-      this.cache = messages;
-      return this.cache;
-    } else {
-      let startIndex = -1;
-      for (let i = this.cache.length - 1, j = messages.length - 1; i >= 0 && j >= 0; i--, j--) {
-        if (this.cache[i] === messages[j]) {
-          startIndex = i;
-        } else {
-          break;
-        }
-      }
+  async newMessages(messages: string[]) {
+    let newMessages: string[] = [];
 
-      console.log("---------------------------------")
-      if (startIndex !== -1 && startIndex + 1 < messages.length) {
-        newMessageList = messages.slice(startIndex)
-         // this.cache = { ...newMessageList };
-        console.log(newMessageList);
-        return newMessageList;
-      } else {
-        console.log("No hay coincidencias consecutivas en las listas.");
-        return newMessageList;
+    if (this.cache.length === 0) {
+      this.cache = [...messages];
+      return [...messages];
+    }
+
+    let found = false;
+    for (let i = 0; i < messages.length; i++) {
+      if (!this.cache.includes(messages[i])) {
+        found = true;
+        newMessages.push(messages[i]);
+      } else if (found) {
+        break;
       }
     }
+
+    if (found) {
+      this.cache = [...messages];
+    }
+
+    return newMessages;
   }
-
-
-
-
 }
