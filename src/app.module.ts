332f17837403813b92ffa706c9fc76723f4cf334
Alexis Dávila <alexisdavila@assertsolutions.com>
2024-03-27 16:10:52 -0300

    Configuracion bot discord

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from "@nestjs/config";
 
+import * as process from "process";
+import { NecordModule } from "necord";
+import { IntentsBitField } from "discord.js";
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Commons } from './commons/commons';
 import { WebadminModule } from './webadmin/webadmin.module';
+import { DiscordModule } from './discord/discord.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    NecordModule.forRoot({
+      token: process.env.DISCORD_TOKEN,
+      intents:[IntentsBitField.Flags.Guilds]
+    }),
     WebadminModule,
+    DiscordModule,
   ],
   controllers: [AppController],
   providers: [AppService, Commons],
