53e8bc7d51ba41efebece2fac203529f689e035c
Alexis Dávila <alexisdavila@assertsolutions.com>
2024-04-17 00:52:05 -0300

    Cambio a url db

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,11 +18,12 @@ import { DiscordBotModule } from './discord-bot/discord-bot.module';
     ScheduleModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      // host: process.env.DB_HOST,
+      // port: +process.env.DB_PORT,
+      // database: process.env.DB_NAME,
+      // username: process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD,
+      url: process.env.DB_URL,
       autoLoadEntities: true,
       synchronize: true, //en prod va en false
     }),
